e7971564c5b3d1c7176540107e0b5e73c070e88d
GIOVANI MARQUES <102267202+DevGiovaniMarques@users.noreply.github.com>
2022-09-19 23:17:12 -0300

    Atualização do dashboard e criação do modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
 import LogoImg from '../../assets/logo.svg';
+
 import { Container, Content } from './style';
 
-export function Header(){
-  return (
+
+interface HeaderProps{
+  onOpenNewTransactionmodal: () => void;
+}
+
+export function Header({onOpenNewTransactionmodal}:HeaderProps){
+
+    return (
     <Container>
       <Content>
         <img src={LogoImg} alt="logo_dtmoney" />
-        <button type="button">
+        <button type="button" onClick={onOpenNewTransactionmodal}>
           Nova Transação
         </button>
       </Content>
---
+++ src/components/TransactionsTable/index.tsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
+import { Container } from "./style";
+
+
+export function TransactionTable(){
+
+  useEffect(()=>{
+    api.get('transactions')
+    .then(response => console.log(response.data))
+  },[])
+
+
+  return(
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Manutenção de website</td>
+            <td className="withdraw">- R$1.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Layout</td>
+            <td className="withdraw">- R$1.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+          <tr>
+            <td>Desenvolvimento de Layout</td>
+            <td className="deposit">R$1.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2022</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  )
+}
